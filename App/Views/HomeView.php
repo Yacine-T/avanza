6bf10cfacc0da84e7b03cbf2f8703ec279c179e5
TAZDAIT YACINE p1702092 <yacine.tazdait@etu.univ-lyon1.fr>
2020-11-20 12:55:51 +0100

    Ajout des icone anvec les liens de redirection vers les comptes FB, insta de l'asso et le cqfd

--- App/Views/HomeView.php
+++ App/Views/HomeView.php
@@ -13,9 +13,9 @@
 <a href="index.php?action=connection">Connectez-vous</a>
 <a href="index.php?action=join">Inscrivez-vous</a>
     <br><br><br>
-<a href="https://fr-fr.facebook.com/avanzaassociation/" target="_blank"><img src="../Public/Images/facebook.png"></a>
-<a href="https://www.instagram.com/avanzalyon1/?hl=fr" target="_blank"><img src="../Public/Images/instagram.png"></a>
-<a href="https://www.instagram.com/avanzalyon1/?hl=fr" target="_blank"><img src="../Public/Images/cqfd.png"></a>
+<a href="https://fr-fr.facebook.com/avanzaassociation/" target="_blank"><img src="../Public/Images/facebook.png" alt="lien vers le compte facebook d'Avanza"></a>
+<a href="https://www.instagram.com/avanzalyon1/?hl=fr" target="_blank"><img src="../Public/Images/instagram.png" alt="lien vers le compte instagram d'Avanza"></a>
+<a href="https://www.instagram.com/avanzalyon1/?hl=fr" target="_blank"><img src="../Public/Images/cqfd.png" alt="lien vers le cqfd"></a>
 
 </body>
 </html>
